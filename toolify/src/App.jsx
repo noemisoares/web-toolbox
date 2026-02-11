1c32d254f9ff8ee33063a96b94908167534c5c8b
Noemi Soares <142548973+noemisoares@users.noreply.github.com>
2026-02-11 14:10:20 +0000

    Estrutura de projeto em react

---
+++ toolify/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Compress from "./pages/Compress";
+import QrCode from "./pages/QrCode";
+import Convert from "./pages/Convert";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/compress" element={<Compress />} />
+        <Route path="/qrcode" element={<QrCode />} />
+        <Route path="/convert" element={<Convert />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
